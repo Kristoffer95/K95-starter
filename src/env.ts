b4966df53c28251fc0b8c1cc811fd85e800b2ad6
kristoffer95 <29176224+Kristoffer95@users.noreply.github.com>
2024-07-17 18:02:08 +0800

    bugfix: fixed env issue

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,9 @@
-import { config } from "dotenv";
-import { expand } from "dotenv-expand";
+/**
+ * Reference: https://env.t3.gg/docs/nextjs
+ */
 
-import { ZodError, z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 const stringBoolean = z.coerce
   .string()
@@ -10,36 +12,93 @@ const stringBoolean = z.coerce
   })
   .default("false");
 
-const EnvSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "test", "production"])
-    .default("development"),
-  DATABASE_URL: z.string().url(),
-  DB_MIGRATING: stringBoolean,
-  DB_SEEDING: stringBoolean,
-  NEXT_PUBLIC_SUPABASE_URL: z.string(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+export const env = createEnv({
+  /**
+   * Specify your server-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars.
+   */
+  server: {
+    DATABASE_URL: z.string().url(),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    DB_MIGRATING: stringBoolean,
+    DB_SEEDING: stringBoolean,
+  },
+
+  /**
+   * Specify your client-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars. To expose them to the client, prefix them with
+   * `NEXT_PUBLIC_`.
+   */
+  client: {
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  },
+
+  /**
+   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
+   * middlewares) or client-side so we need to destruct manually.
+   */
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
 });
 
-export type EnvSchema = z.infer<typeof EnvSchema>;
+// import { config } from "dotenv";
+// import { expand } from "dotenv-expand";
 
-// expand(config());
-// expand(config({ path: ".env.local" }));
+// import { ZodError, z } from "zod";
 
-try {
-  EnvSchema.parse(process.env);
-} catch (error) {
-  if (error instanceof ZodError) {
-    let message = "Missing required values in .env:\n";
-    error.issues.forEach((issue) => {
-      message += issue.path[0] + "\n";
-    });
-    const e = new Error(message);
-    e.stack = "";
-    throw e;
-  } else {
-    console.error(error);
-  }
-}
+// const stringBoolean = z.coerce
+//   .string()
+//   .transform((val) => {
+//     return val === "true";
+//   })
+//   .default("false");
 
-export const env = EnvSchema.parse(process.env);
+// const EnvSchema = z.object({
+//   NODE_ENV: z
+//     .enum(["development", "test", "production"])
+//     .default("development"),
+//   DATABASE_URL: z.string().url(),
+//   DB_MIGRATING: stringBoolean,
+//   DB_SEEDING: stringBoolean,
+//   NEXT_PUBLIC_SUPABASE_URL: z.string(),
+//   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+// });
+
+// export type EnvSchema = z.infer<typeof EnvSchema>;
+
+// // expand(config());
+// // expand(config({ path: ".env.local" }));
+
+// try {
+//   EnvSchema.parse(process.env);
+// } catch (error) {
+//   if (error instanceof ZodError) {
+//     let message = "Missing required values in .env:\n";
+//     error.issues.forEach((issue) => {
+//       message += issue.path[0] + "\n";
+//     });
+//     const e = new Error(message);
+//     e.stack = "";
+//     throw e;
+//   } else {
+//     console.error(error);
+//   }
+// }
+
+// export const env = EnvSchema.parse(process.env);
